fd6ce01500a87d9122571f1f1d660934ffbdb538
David Markov <Markov.David@seznam.cz>
2023-03-20 15:07:42 +0100

    create CLI options parser

--- core/src/main/java/cz/zcu/kiv/nlp/ir/Main.java
+++ core/src/main/java/cz/zcu/kiv/nlp/ir/Main.java
@@ -1,14 +1,25 @@
 package cz.zcu.kiv.nlp.ir;
 
+import org.apache.commons.cli.HelpFormatter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import cz.zcu.kiv.nlp.ir.command.CommandParser;
 import cz.zcu.kiv.nlp.ir.tokenizer.DefaultTokenizer;
 import cz.zcu.kiv.nlp.ir.tokenizer.Tokenizer;
 
 public class Main {
 
   public static void main(final String[] args) {
+    final var parser = new CommandParser(LoggerFactory.getILoggerFactory());
+    final var config = parser.parse(args);
+
+    if (config.isJustPrintHelp()) {
+      final var formatter = new HelpFormatter();
+      formatter.printHelp("search-engine", parser.getOptions());
+      return;
+    }
+
     final Logger logger = LoggerFactory.getLogger(Main.class);
     final Tokenizer tokenizer = new DefaultTokenizer(LoggerFactory.getILoggerFactory());
     tokenizer.tokenize("Ahoj, světe.");
